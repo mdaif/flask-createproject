438dd56cb37f58e3c539896e2be427f04881d859
mohamed.daif <mohamed.daif@olx.com>
2022-05-23 14:36:49 +0200

    release: bump up the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='createproject-mdaif',
-    version='0.0.1',
+    version='0.0.2',
     author='Mohamed Daif',
     author_email='daif.control@gmail.com',
     description='A utility program to bootstrap flask projects',
